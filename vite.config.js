d7220972f4c9517a80c99ae50837609e106c6662
Elizar Aitbek <elizar.aitbek@gmail.com>
2023-06-03 17:06:46 +0600

    fix svgr error

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    svgr({
-      svgrOptions: {
-        exportAsDefault: true,
-      },
-    }),
-  ],
+  plugins: [react()],
 });
