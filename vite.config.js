215ed09c1cbce20d2973c34b44c290cd74616c5b
Elizar Aitbek <elizar.aitbek@gmail.com>
2023-06-04 21:11:50 +0600

    save

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import viteCompression from "vite-plugin-compression";
-import "vite/modulepreload-polyfill";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteCompression()],
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: "/path/to/main.js",
-    },
-  },
+  plugins: [react()],
 });
